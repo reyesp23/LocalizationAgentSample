997dac1050280e6f1f4b0d0ae455e7ab66543010
Patricio Reyes <reyes_p23@hotmail.com>
2024-03-01 19:50:22 -0800

    Add dummy project

---
+++ LocAgent/LocAgent/LocAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocAgentApp.swift
+//  LocAgent
+//
+//  Created by Patricio Reyes on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LocAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
